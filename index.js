77905ac8efd312fe4616f7aace69cd4ad82998cb
berylann <berylaann@gmail.com>
2024-08-08 18:50:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "berylaann@gmail.com",
+        Password : "BMoi2504!aBDZ",
+        To : 'contact@apiltd.co',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+ 
+ }
